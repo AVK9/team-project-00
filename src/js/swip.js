8a5c9ee359da231df086d246f6ea8969881c1b21
AlexKat <katsubo79@gmail.com>
2023-08-01 20:51:36 +0300

    swip

---
+++ src/js/swip.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 3,
+  spaceBetween: 16,
+   loop: true,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 0
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16
+    },
+    // when window width is >= 640px
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 16
+    }
+    },
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+})
