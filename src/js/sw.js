a216fb428edb056c216503f70becd4be2a03f85f
AlexKat <katsubo79@gmail.com>
2023-08-02 22:08:20 +0300

    sw

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,12 +1,23 @@
 const swiper = new Swiper('.swiperq', {
- direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 16,
+  autoplay: {
+  delay: 5000,
+  },
   loop: true,
- 
+    breakpoints: {
+      768: {
+      slidesPerView: 2,
+      spaceBetween: 16
+      },
+              1280: {
+      slidesPerView: 3,
+      spaceBetween: 32
+    }
+    },
   pagination: {
     el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: 'true',
   },
-  //  navigation: {
-  //   nextEl: '.swiper-button-next',
-  //   prevEl: '.swiper-button-prev',
-  // },
 })
--- src/js/swip.js
+++ src/js/swip.js
@@ -1,25 +1,20 @@
 const swiper = new Swiper('.swiper', {
-  // Default parameters
   slidesPerView: 3,
   spaceBetween: 16,
   loop: true,
-    autoplay: {
-   delay: 5000,
+  autoplay: {
+  delay: 5000,
  },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
+    breakpoints: {
+        320: {
       slidesPerView: 1,
       spaceBetween: 0
     },
-    // when window width is >= 480px
-    768: {
+        768: {
       slidesPerView: 2,
-      spaceBetween: 0
+      spaceBetween: 16
     },
-    // when window width is >= 640px
-    1280: {
+        1280: {
       slidesPerView: 3,
       spaceBetween: 16
     }
